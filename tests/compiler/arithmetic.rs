055cc54d992e5be1f149ecdaa36fbebc37c524fe
ffwff <mogamiriver@tuta.io>
2020-03-28 22:19:10 +0700

    add imul/idiv tests

--- tests/compiler/arithmetic.rs
+++ tests/compiler/arithmetic.rs
@@ -114,3 +114,43 @@ fn sub_i64() {
     record(f(10i64, 15i64))
     ", runtime);
 }
+
+#[cfg(test)]
+#[test]
+fn imul_i64() {
+    extern "C" fn record_i64(n: i64) {
+        assert_eq!(n, 150);
+    }
+    let mut runtime = Runtime::empty();
+    runtime.insert_func("record_i64", record_i64 as extern "C" fn(i64));
+    utils::parse_and_run("
+    @[Static(record_i64)]
+    def record(n: I64): Nil
+    end
+
+    def f(x, y)
+        return x * y
+    end
+    record(f(10i64, 15i64))
+    ", runtime);
+}
+
+#[cfg(test)]
+#[test]
+fn idiv_i64() {
+    extern "C" fn record_i64(n: i64) {
+        assert_eq!(n, 20);
+    }
+    let mut runtime = Runtime::empty();
+    runtime.insert_func("record_i64", record_i64 as extern "C" fn(i64));
+    utils::parse_and_run("
+    @[Static(record_i64)]
+    def record(n: I64): Nil
+    end
+
+    def f(x, y)
+        return x / y
+    end
+    record(f(40i64, 2i64))
+    ", runtime);
+}
